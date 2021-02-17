93683ca51040ea08b2925e9fbaa928e926d8d7ed
guoxw <xuewei_g@163.com>
2021-02-17 17:17:22 +0800

    add

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,13 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello,World!");
+        Thread thread =new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Thread start");
+            }
+        });
+        thread.start();
+        
     }
 }
